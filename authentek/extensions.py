b3c08c5a3d9d7728e2e5b3a31e630d7227b5c164
Omar Shaban <omars@php.net>
2020-06-17 15:19:23 +0200

    logger

--- authentek/extensions.py
+++ authentek/extensions.py
@@ -3,13 +3,16 @@
 All extensions here are used as singletons and
 initialized in application factory
 """
+
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from authentek.logger import log
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
 cors = CORS(resources={r"/v1/*": {"origins": "*"}})
+log.debug("in debug mode")
