cf64c783b3efa5a772bd4d3b4e8df0976662f298
Omar Shaban <omars@php.net>
2021-07-01 08:58:19 +0200

    fix wait-for-pgsql script

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 export FLASK_APP=authentek/app.py
 export APP_SETTINGS="authentek.server.config.DevelopmentConfig"
-#export SECRET_KEY="\x04:\xe7\x17*\x0f\xcf\xa5=\x0b\xd8H8_\x8d\x14\xa6\x98w\xa9{\x15\x84$"
-#export PG_URI="postgresql://test:test@postgres:5432/"
 set -e
 
 export PYTHONPATH=.:$PYTHONPATH
@@ -18,7 +16,7 @@ export PYTHONPATH=.:$PYTHONPATH
 RETRIES=10
 
 until psql -h $PG_HOST -U $PG_USER -d $PG_DATABASE -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
-  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  echo "Waiting for postgres server to start, $((RETRIES)) remaining attempts..."   RETRIES=$((RETRIES-=1))
   sleep 1
 done
 
